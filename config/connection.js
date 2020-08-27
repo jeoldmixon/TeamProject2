ff25a6bffa390d370b30fa6a1fbf9765a1766691
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-27 18:51:32 -0500

    added new dependencies

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize')
+
+require('dotenv').config();
+
+
+// create connection to our database, pass in your MySQL information for username and password
+let sequelize;
+//this is for heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    //this is for local database
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+module.exports = sequelize
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS job_finder_db;
+
+CREATE DATABASE job_finder_db;
