558441d4ac935e20bfbf3ab002bcc4951f3955f1
jeoldmixon <jeoldmixon@yahoo.com>
2020-09-01 22:45:19 -0500

    login-homepage-signup added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,53 +1,40 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const sequelize = require('../config/connection');
-const { Search, User } = require('../models');
+const { Search, User } = require("../models");
 
-router.get('/', (req, res) => {
-    res.render('dashboard')
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-// router.get('/', (req, res) => {
-//     Search.findAll({
-//         attributes: [
-//             'id',
-//             'url',
-//             'company_name',
-//             'title',
-//             'salary',
-//             'location'
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbSearchData => res.json(dbSearchData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+  res.render("login");
+});
 
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect('/');
-//         return;
-//     }
-    
-//     res.render('login');
-// });
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-// router.get('/signup', (req, res) => {
-// 	if (req.session.loggedIn) {
-// 		res.redirect('/');
-// 		return;
-// 	}
-	
-// 	res.render('signup');
-// });
+  res.render("signup");
+});
+
+router.get("/dashboard", (req, res) => {
+  // Search.findAll({
+  //   attributes: ["id", "url", "company_name", "title", "location"],
+  // });
+  res.render("dashboard");
+  // .then((dbSearchData) => res.json(dbSearchData))
+  // .catch((err) => {
+  //   console.log(err);
+  //   res.status(500).json(err);
+  // });
+});
 
 // router.get('/:id', (req, res) => {
 //     Search.findOne({
@@ -82,7 +69,6 @@ router.get('/', (req, res) => {
 //         });
 // });
 
-// 
-
+//
 
 module.exports = router;
